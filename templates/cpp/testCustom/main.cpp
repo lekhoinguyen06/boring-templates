4d26919d1ad975491d2c40a0c5fd2ef203b309ce
Le Khoi Nguyen <155555943+lekhoinguyen06@users.noreply.github.com>
2026-01-19 18:35:47 +0700

    remove testCustom

--- templates/cpp/testCustom/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <fmt/core.h>
-
-int main() {
-  fmt::print("Application starting...\n");
-  return 0;
-}
--- templates/cpp/testCustom/watch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Watch for changes in source files and rebuild
-# Install with: brew install watchexec
-watchexec -c -e cpp,h --ignore build -- ./dev.sh
